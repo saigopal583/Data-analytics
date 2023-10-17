9b23b3b958491daf132707de8c2e70049c009405
saigopal583 <147130630+saigopal583@users.noreply.github.com>
2023-10-17 10:16:23 +0530

    Add files via upload

---
+++ Project Files/Scaffold/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = False, port = 8000)
